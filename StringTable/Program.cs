62b8221af261b6e579b612d3ddc259346e0f230e
Michael Elly <michael.elly@live.com>
2021-05-17 22:17:15 +0300

    Add project files.

---
+++ StringTable/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace StringTableDemo {
+	class Program {
+		static void Main(string[] args) {
+			StringTable t = new StringTable(new string[] { "A", "BBB", "C", "D" }) { SortSyntax = "A", AddIndexLineColumn = true, Indentation = 10 };
+			t.AddRow(new object[] { "assdf", "sd", "Save your favorite articles to read offline.", "DD" });
+			t.AddRow(new object[] { "csdfsdf", "We are the World", "The English Wikipedia is the English language edition of the free online encyclopedia.", "DD" });
+			t.AddRow(new object[] { "dsdfsdf", 4, "4", "DD" });
+			t.AddRow(new object[] { "esdfsdf", 5, "", "DD" });
+			t.AddRow(new object[] { "bsdfsdf", "Lez Zepplin", "22111324", "DD" });
+
+			int i = 0;
+			Console.WriteLine($"Sample {++i}");
+			Console.WriteLine(t.CompileTable());
+
+			Console.WriteLine($"Sample {++i}");
+			t.UpdateColumn(2, StringTable.WrapText.TrimEnd, 22);
+			Console.WriteLine(t.CompileTable());
+
+			Console.WriteLine($"Sample {++i}");
+			t.UpdateColumn(2, StringTable.WrapText.TrimStart, 22);
+			Console.WriteLine(t.CompileTable());
+
+			Console.WriteLine($"Sample {++i}");
+			t.UpdateColumn(2, StringTable.WrapText.Wrap, 22);
+			Console.WriteLine(t.CompileTable());
+
+			Console.WriteLine($"Sample {++i}");
+			t.UpdateColumn(2, StringTable.WrapText.TrimEnd, 22);
+			t.UpdateColumn(1, StringTable.WrapText.Wrap, 5);
+			Console.WriteLine(t.CompileTable());
+
+			Console.WriteLine($"Sample {++i}");
+			t.UpdateColumn(2, StringTable.WrapText.Wrap, 22);
+			t.UpdateColumn(1, StringTable.WrapText.Wrap, 8);
+			Console.WriteLine(t.CompileTable());
+
+			Console.WriteLine($"Sample {++i}");
+			t.UpdateColumn(2, StringTable.WrapText.TrimEnd, 200);
+			Console.WriteLine(t.CompileTable());
+
+			Console.WriteLine($"Sample {++i}");
+			t.AddIndexLineColumn = false;
+			Console.WriteLine(t.CompileTable());
+		}
+	}
+}
---
+++ StringTable/StringTable.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+public class StringTable {
+	public string SortSyntax { get; set; }
+	public int Indentation { get; set; } = 0;
+	public bool AddIndexLineColumn { get; set; } = false;
+	public bool DrawHeader { get; set; } = true;
+	public bool UnderlineHeader { get; set; } = true;
+	public int RedrawHeaderAfterRows { get; set; } = 60;
+	public string LineBreakingText { get; set; } = "";
+
+	public enum WrapText {
+		Wrap,
+		TrimStart,
+		TrimEnd
+	}
+
+	private List<(string name, WrapText wrapText, int maxColWidth)> mColumns = new List<(string name, WrapText wrapText, int maxColWidth)>();
+	private List<object[]> mRowVals = new List<object[]>();
+
+	public void UpdateColumn(int colIndex, WrapText wrapText, int maxColWidth) {
+		if (colIndex < mColumns.Count && colIndex >= 0) {
+			string name = mColumns[colIndex].name;
+			mColumns[colIndex] = (name, wrapText, maxColWidth);
+		}
+	}
+
+	public StringTable(string[] columnNames) {
+		for (int i = 0; i < columnNames.Length; i++) {
+			mColumns.Add((columnNames[i], WrapText.TrimStart, int.MaxValue));
+		}
+	}
+
+	public void AddRow(object[] row_elements) {
+		mRowVals.Add(row_elements);
+	}
+
+	public string CompileTable() {
+		int i; // row index
+
+		// check we have some value
+		if (mColumns.Count <= 0) return "Error: No table columns defined.";
+		if (mRowVals.Count <= 0) return "Error: No table rows defined.";
+
+		// setup the datatable
+		DataTable d = new DataTable();
+		try {
+			for (i = 0; i < mColumns.Count; i++) d.Columns.Add(mColumns[i].name);
+			for (i = 0; i < mRowVals.Count; i++) d.Rows.Add(mRowVals[i]);
+		} catch (Exception ex) {
+			return $"Error constructing DataTable: {ex.Message}";
+		}
+
+		// construct the string
+		string CrLf = Environment.NewLine;
+		int table_columns_count = d.Columns.Count;
+		StringBuilder s = new StringBuilder(); // whole report text
+		string h = ""; // header_text
+		int[] col_width = new int[table_columns_count];
+		int[] col_start = new int[table_columns_count];
+		string indent_str = "".PadRight(Indentation);
+		int index_column_width = d.Rows.Count.ToString().Length + 1;
+
+		if (RedrawHeaderAfterRows < 5) RedrawHeaderAfterRows = 5;
+
+		try {
+			// evaluate the optimal column width
+			if (DrawHeader) {
+				for (int j = 0; j <= table_columns_count - 1; j++) {
+					col_width[j] = d.Columns[j].ColumnName.Length;
+				}
+			}
+			for (i = 0; i <= d.Rows.Count - 1; i++) {
+				for (int j = 0; j <= table_columns_count - 1; j++) {
+					int cl = d.Rows[i][j].ToString().Length;
+					if (cl > mColumns[j].maxColWidth) {
+						col_width[j] = mColumns[j].maxColWidth;
+						continue;
+					} else if (cl > col_width[j]) {
+						col_width[j] = cl;
+					}
+				}
+			}
+			for (int j = 0; j <= table_columns_count - 1; j++) {
+				col_width[j] += 1;
+			}
+
+			for (int j = 0; j <= table_columns_count - 1; j++) {
+				if (j == 0) {
+					col_start[j] = Indentation + (AddIndexLineColumn ? index_column_width : 0);
+				} else {
+					col_start[j] = col_start[j - 1] + col_width[j - 1];
+				}
+			}
+
+			// now build the header string
+			if (DrawHeader) {
+				s.Append(indent_str);
+				if (AddIndexLineColumn) s.Append("#".PadRight(index_column_width));
+				for (int j = 0; j <= table_columns_count - 1; j++) {
+					s.Append(d.Columns[j].ToString().PadRight(col_width[j]));
+				}
+				s.Append(CrLf);
+				if (UnderlineHeader) {
+					s.Append(indent_str);
+					if (AddIndexLineColumn) s.Append("".PadRight(index_column_width - 1, '-').PadRight(index_column_width));
+					for (int j = 0; j <= table_columns_count - 1; j++) {
+						s.Append(new string(' ', d.Columns[j].ToString().PadRight(col_width[j]).Length - 1).Replace(" ", "-") + " ");
+					}
+					s.Append(CrLf);
+				}
+				h = s.ToString();
+				s.Clear(); // we're going to rebuild the report including the header
+			}
+
+			// create the dataview and sort it
+			DataView v = d.DefaultView;
+			if (!string.IsNullOrWhiteSpace(SortSyntax)) v.Sort = SortSyntax;
+
+			// calculate lines per row			
+			Dictionary<(int rowIndex, int colIndex), int> lines_per_cell = new Dictionary<(int rowIndex, int colIndex), int>();
+			Dictionary<int, int> lines_per_row = new Dictionary<int, int>();
+			i = -1; // row index zero based			
+			foreach (DataRowView r in v) {
+				i++;
+				lines_per_row[i] = 1;
+				for (int j = 0; j <= table_columns_count - 1; j++) {
+					lines_per_cell[(i, j)] = 1;
+					if (r[j].ToString().Length > col_width[j] && mColumns[j].wrapText == WrapText.Wrap) {
+						int total_cell_len = r[j].ToString().Length;
+						lines_per_cell[(i, j)] = Math.Max(1, (int)Math.Ceiling((double)total_cell_len / (col_width[j] - 1)));
+						lines_per_row[i] = Math.Max(lines_per_row[i], lines_per_cell[(i, j)]);
+					}
+				}
+			}
+
+			// build the table string
+			i = -1; // row index zero based			
+			foreach (DataRowView r in v) {
+				i++;
+				if (i % RedrawHeaderAfterRows == 0) {
+					if (i == 0) {
+						s.Append(h);
+					} else {
+						s.Append($"{LineBreakingText}{CrLf}{h}");
+					}
+				}
+				for (int u = 0; u < lines_per_row[i]; u++) {
+					s.Append(indent_str);
+					if (u == 0) {
+						if (AddIndexLineColumn) s.Append((i + 1).ToString().PadRight(index_column_width));
+						for (int j = 0; j <= table_columns_count - 1; j++) {
+							if (r[j].ToString().Length > col_width[j]) {
+								if (mColumns[j].wrapText == WrapText.Wrap) {
+									s.Append(Token(r[j].ToString(), u, col_width[j] - 1));
+								} else if (mColumns[j].wrapText == WrapText.TrimEnd) {
+									s.Append($"{r[j].ToString().Substring(0, col_width[j] - 3)}.. ");
+								} else {
+									s.Append(($"..{r[j].ToString().Substring(r[j].ToString().Length - col_width[j] + 3)}").PadRight(col_width[j]));
+								}
+							} else {
+								s.Append(r[j].ToString().PadRight(col_width[j]));
+							}
+						}
+					} else {
+						if (AddIndexLineColumn) s.Append("".PadRight(index_column_width));
+						for (int j = 0; j <= table_columns_count - 1; j++) {
+							if (mColumns[j].wrapText == WrapText.Wrap && lines_per_cell[(i, j)] > u) {
+								s.Append(Token(r[j].ToString(), u, col_width[j] - 1));
+							} else {
+								s.Append("".PadRight(col_width[j]));
+							}
+						}
+					}
+					s.Append(CrLf);
+				}
+			}
+
+			// we've completed it 
+			return s.ToString();
+
+		} catch (Exception ex) {
+			return $"Error builing table: {ex.Message} {ex.StackTrace}";
+		}
+	}
+
+	private string Token(string source, int segmentIndex, int segmentLength) {
+		if (source.Length <= segmentLength) {
+			return source.PadRight(segmentLength + 1);
+		} else if (source.Length > segmentLength * segmentIndex) {
+			return source.Substring(segmentIndex * segmentLength, source.Length - segmentIndex * segmentLength >= segmentLength ? segmentLength : source.Length - segmentIndex * segmentLength).PadRight(segmentLength + 1);
+		} else {
+			return "".PadRight(segmentLength + 1);
+		}
+	}
+}
+
